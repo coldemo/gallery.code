da6bcccddf810d2ac2b1714345e80a0c9ded2ef5
Fritz Lin <uxfritz@163.com>
2020-04-11 18:45:08 +0800

    wip: add genji demo

---
+++ public/code/genji.jsx
@@ -0,0 +1,126 @@
+await loadScript('https://unpkg.com/react-redux')
+await loadScript('https://unpkg.com/genjijs')
+
+let { Provider, useSelector, useDispatch } = ReactRedux
+
+const numberUnit = {
+  namespace: 'number',
+  state: {
+    num: 0,
+    desc: {
+      num: 0
+    }
+  },
+  actionCreators: {
+    add({ type, payload }, { getState, pick, save }) {
+      const num = pick('num');
+      save({ num: num + payload });
+    },
+    async saveAsync(action, { getState, pick, save }) {
+      return fetch('/mock')
+        .then(response => response.json())
+        .then(data => {
+          save({ num: data.saveNum });
+          save({ desc: { num: data.saveNum } });
+        })
+        .catch(e => {
+          console.error('fetch error:', e);
+        });
+    }
+  }
+};
+const userUnit = {
+  namespace: 'user',
+  state: {
+    name: 'zhangsan',
+    num: 0
+  },
+  actionCreators: {
+    async saveOther(action, { getState, pick, save }) {
+      return fetch('/mock')
+        .then(response => response.json())
+        .then(data => {
+          save({ num: data.saveNum + pick('num', 'number') }, 'number');
+        })
+        .catch(e => {
+          console.error('fetch error:', e);
+        });
+    }
+  }
+};
+
+const genji = new Genji({ injectAsyncLoading: true, autoUpdateAsyncLoading: true });
+const unitTypes = {
+  numberUnit: genji.model(numberUnit),
+  userUnit: genji.model(userUnit),
+}
+genji.start();
+const store = genji.getStore()
+
+let GenjiApp = () => {
+  let dispatch = useDispatch()
+  let props = {
+    ...useSelector(state => state),
+    add: () => {
+      dispatch({
+        type: unitTypes['numberUnit'].add,
+        payload: 1
+      });
+    },
+    saveAsync: () => {
+      dispatch({
+        type: unitTypes['numberUnit'].saveAsync
+      });
+    },
+    saveOther: () => {
+      dispatch({
+        type: unitTypes['userUnit'].saveOther
+      });
+    }
+  }
+
+  useEffect(() => {
+    props.saveAsync();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      {props.number.saveAsyncLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div onClick={props.saveAsync}>init num from mock (click me)</div>
+      )}
+      <div onClick={props.add}>action test (click me)</div>
+      {props.user.saveOtherLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div onClick={props.saveOther}>save num from other model (click me)</div>
+      )}
+      <div>current number is: {props.number.num}</div>
+    </div>
+  )
+}
+
+let App = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <div>
+        <h1>Genji</h1>
+        <a target="_blank" href="https://github.com/huangruichang/genjijs">
+          https://github.com/kelekexiao123/genjijs
+        </a>
+        <br /><br />
+      </div>
+      <Provider store={store}>
+        <GenjiApp />
+      </Provider>
+    </div>
+  )
+}
+
+let sleep = (ms) => new Promise(r => setTimeout(r, ms))
+let fetch = async () => {
+  await sleep(1000)
+  return { "message": "ojbk", "saveNum": 100 }
+}
